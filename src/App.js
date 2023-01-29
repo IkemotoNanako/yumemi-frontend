6573643a541581b3bb7a528af17abcc11c16a594
IkemotoNanako <chibencaizi@gmail.com>
2023-01-29 19:38:57 +0900

    Highchartsでのグラフデモ

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+
+const options = {
+  title: {
+      text: 'My chart'
+  },
+  series: [{
+      type: 'line',
+      data: [1, 2, 3]
+  }]
+};
 function App() {
   return (
-    <p>React App</p>
+    <div>
+      <p>React App</p>
+      <HighchartsReact
+    highcharts={Highcharts}
+    options={options}
+  />
+    </div>
   );
 }
 
