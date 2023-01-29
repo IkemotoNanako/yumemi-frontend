27b0ac3eb6aab96983c44709614a4861bd64159f
IkemotoNanako <chibencaizi@gmail.com>
2023-01-29 23:46:13 +0900

    チェックボックスの動的生成

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
-import React from 'react';
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
+import React , { Component } from 'react';
 
-const options = {
-  title: {
-      text: 'My chart'
-  },
-  series: [{
-      type: 'line',
-      data: [1, 2, 3]
-  }]
-};
-function App() {
-  return (
-    <div>
+class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      selected: Array(47).fill(false),
+      prefectures: {},
+      series: []
+    };
+  }
+  _ChangeSelection(index){
+    const selected_copy = this.state.selected.slice();
+    selected_copy[index] = !selected_copy[index];
+    this.setState({selected:selected_copy})
+  }
+  componentDidMount() {
+    fetch('https://opendata.resas-portal.go.jp/api/v1/prefectures', {
+      headers: { 'X-API-KEY': "SvJv1CMqnWejMMPeZTf6HYxJMeJsxRwu9eKUeZO9" }
+    })
+      .then(response => response.json())
+      .then(res => {
+        this.setState({ prefectures: res.result });
+      });
+  }
+  render(){
+    const obj = this.state.prefectures;
+    const options = {
+      title: {
+          text: 'My chart'
+      },
+      series: [{
+          type: 'line',
+          data: [1, 2, 3]
+      }]
+    };
+    return (
       <div>
-        <input type="checkbox" checked/>
-        <label>label</label>
+        <div>
+          {Object.keys(obj).map((key) => 
+            <div key={obj[key].prefCode}>
+              <input type="checkbox" onChange={() => this._ChangeSelection(obj[key].prefCode - 1)} checked={this.state.selected[obj[key].prefCode - 1]}/>
+              <label>{obj[key].prefName}</label>
+            </div>
+          )}
+        </div>
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={options}
+        />
       </div>
-      <HighchartsReact
-        highcharts={Highcharts}
-        options={options}
-      />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
